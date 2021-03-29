d51c864ac1601f2b2620a07599ac61cb96604390
Raz <razaljaf@gmail.com>
2021-03-29 10:31:57 -0600

    login page done

--- App.js
+++ App.js
@@ -1,38 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-native';
 import LoginForm from './components/LoginForm'
 
 export default function App() {
   const [user, setUser] = useState({})
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {user.username
         ? <Text style={styles.text}> Welcome {user.username}</Text>
         : <LoginForm setUser={setUser}/>
+
       }
-      <Button title={} />
-    </View>
+                      <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setUser({})}>
+                <Text>Return</Text>
+                </TouchableOpacity>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E5E5E5',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: 'space-around',
-  },
-  text: {
+  Text: {
     color: "black",
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
-  }
+  },
+  button: {
+    marginLeft: 150,
+    alignItems: 'center',
+    backgroundColor: "#FFFFFF",
+    padding: 10,
+    borderRadius: 60,
+    width: 130,
+    marginTop: 160,
+    opacity: 0.5
+  },
 });
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState }from 'react'
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import { Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 
 export default function LoginForm(props) {
 
@@ -14,33 +14,76 @@ export default function LoginForm(props) {
         })
     }
     return (
-        <View style={StyleSheet.form}>
+        <ScrollView style={StyleSheet.form}>
+            <Image source={require('/Users/razsarwar/flatiron/capstone/pema-frontend/assets/pema-login.png')} style={styles.Image} />
+            <Text style={styles.Text}> P E M A</Text>
             <TextInput
                 onChangeText={(text) => setUsername(text)}
                 style={styles.loginInput}
-                placeholder="Username"
+                placeholder="   Username"
                 value={username}
                 />
             <TextInput
                 onChangeText={(text) => setPassword(text)}
                 style={styles.loginInput}
-                placeholder="Password"
+                placeholder="   Password"
                 value={password}
                 secureTextEntry={true}
                 />
-                <Button title="Submit" onPress={handleSubmit}/>
-        </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleSubmit}>
+                <Text>Log In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setUser({})}>
+                <Text>Return</Text>
+                </TouchableOpacity>
+        </ScrollView>
     )
 }
+
 const styles = StyleSheet.create({
     form: {
-        flex: 1
+        backgroundColor: '#ffffff',
     },
     loginInput: {
-        fontSize: 45,
-        backgroundColor: 'beige',
-        borderRadius: 40,
-        margin: 12,
-        borderColor: 'black'
+        marginLeft: 160,
+        marginBottom: -90,
+        marginTop: 100,
+        flex: 0.5,
+        fontSize: 20,
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 5,
+        borderTopRightRadius: 5,
+        borderTopLeftRadius: 20,
+        width: 150,
+    },
+    Text: {
+        margin: 140,
+        marginTop: -450,
+        textAlign: 'center',
+        fontSize: 20,
+        width: 150
+    },
+    button: {
+        marginLeft: 150,
+        alignItems: 'center',
+        backgroundColor: "#FFFFFF",
+        padding: 10,
+        borderRadius: 60,
+        width: 130,
+        marginTop: 160,
+        opacity: 0.5
+    },
+    Image: {
+        flex: 3,
+        resizeMode: 'cover',
+        opacity: 0.5,
+        width: 420,
+        height: 600,
+        borderBottomRightRadius: 220,
+        borderBottomLeftRadius: 220
     }
 })
